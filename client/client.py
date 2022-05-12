e1dd755bcc8c453fc33b6f9a5c81ec20c23ff504
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-12 21:33:54 +0200

    create base client application

---
+++ client/client.py
@@ -0,0 +1,39 @@
+import socket as sk
+import time
+
+
+# Create socket UDP
+sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+
+server_address = ('localhost', 10000)
+message = "I'm a user"
+response = ""
+
+try:
+    # send message to server
+    print ('sending "%s"' % message)
+    time.sleep(2)
+    sent = sock.sendto(message.encode(), server_address)
+
+    # Wait server receive
+    print('waiting to receive from')
+    data, server = sock.recvfrom(4096)
+    response = data.decode('utf8')
+    # Print server info
+    time.sleep(2)
+    print ('received message "%s"' % response)
+
+    while True:
+        # Get command in input and send it to the server
+        message = input("Write a command: ")
+        sent = sock.sendto(message.encode(), server_address)
+        print("Message inviated")
+
+        # Wait again a server message
+        print("Waiting a response")
+        data, server = data, server = sock.recvfrom(4096)
+        response = data.decode('utf8')
+        print ('received message "%s"' % response)
+
+except Exception as info:
+    print(info)
