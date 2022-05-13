fba1a3fca12b884ac4b6b9ad972e8d720e01262e
Dilaver Shtini <dilaver.shtini@studio.unibo.it>
2022-05-13 13:36:59 +0200

    fix bugs and complete command list

--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@ Dilaver Shtini
 import socket as sk
 #per gestire i file
 import os
+import time
 
 # Creo il socket
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
@@ -24,36 +25,41 @@ while True:
     
     if data:
         menu = '\r\nBenvenuto sul server\r\n\r\nOpzioni Disponibili\r\n\r\n1. Visualizzazione dei file dispoinibli\r\n2. Download file\r\n3. Upload file\r\n4. Esci\r\n'         
-        #time.sleep(2)
+        time.sleep(2)
         sent = sock.sendto(menu.encode(), address)
-        print ('Sent menu back to %s' % (address))
+        #print ('Sent menu back to %s' % (address))
 
-        choice = sock.recvfrom(1024).decode()
-        
         while True:
+            choice, address = sock.recvfrom(1024)
+            
+            print('dentro while, choice -> "%s"' % choice.decode())
+            
             # visualizza i file presenti sul server
-            if choice[0] == '1':
-                print('Visualizzazione file\n:')
-                data = os.listdir(path)
+            if choice.decode() == '1':
+                print('Visualizzazione file:\n')
+                data = str( os.listdir(path) )
+                time.sleep(4)
                 #for file in data:
                     #print(file)
                 
                 # download di un file sul server
-            elif choice[0] == '2':
-                print('Download file\n:')
-        
+            elif choice.decode() == '2':
+                print('Download file:\n')
+                
                 # upload di un file sul server
-            elif choice[0] == '3':
-                print('Upload file\n:')
+            elif choice.decode() == '3':
+                print('Upload file:\n')
             
                 # eseguo la funzione di Exit
-            elif choice[0] == '4':
-                print('Exit\n:')
+            elif choice.decode() == '4':
+                print('Exit\n')
+                data = "Exit"
+                sock.sendto(data.encode(), address);
                 break;
             else:
                 data = menu
                     # restituisce le opzioni del menù al client
-            sock.send(data.encode());
+            sock.sendto(data.encode(), address);
         # chiude la connessione
         sock.close()
-
+        break;
