4b86e8dcd398501edc49eb8cee74c4b99b5db2da
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-17 09:57:55 +0200

    fix some problems in uploading

--- client/client_utils.py
+++ client/client_utils.py
@@ -2,6 +2,7 @@ import socket as sk
 import time
 import os
 import threading
+import pickle
 from packet import Packet
 
 PACKET_SIZE_UPLOAD = 32768
@@ -21,27 +22,32 @@ def __upload__(sock, filename, address):
             file.close()
             break
         chunk = Packet(i, packet_body)
-        # Isertion of packets in the list 
+        # Insertion of packets in the list 
         file_packets.append(chunk)
         i = i + 1
 
     # Make the header message
     num_packets = len(file_packets)
-    message = '{num_packets}, {filesize}'
+    message = '{num_packets}'
 
     # Send header file
-    try: 
-        while True:
+    while True:
+        try: 
+            sock.sendto(message.encode(), address)
+            # Catch server response
+            response = sock.recvfrom(4096)
+        except Exception as info:
             sock.sendto(message, address)
-    except Exception as info:
-        sock.sendto(message, address)
 
-    # Catch server response
-    response = sock.recvfrom(4096)
+        if response == 'Header arrived':
+            break
 
-    if response == 'Header arrived':
-        print(response) #print a caso per non dare problemi
-        # Inizio a creare thread per inviare i pacchetti
+    # Start uploading 
+    print('Uploading..')
+    for packet in file_packets :
+        sock.sendto(pickle.dumps(packet), address) # It might throw exceptions
+        print("funziona")
+        response = sock.recvfrom(4096)
 
 
 def __download__(sock, filename, address):
@@ -50,7 +56,7 @@ def __download__(sock, filename, address):
     # Receive packet from server and write to the file
     data, address = sock.recvfrom(PACKET_SIZE_DOWNLOAD)
     packet.write(data.decode('utf8'))
+
     packet.close()
     # Send a packet
     print("Finished!")
-
--- client/packet.py
+++ client/packet.py
@@ -6,8 +6,3 @@ class Packet :
         self.packet_number = number
         self.body = body
         self.checksum = hashlib.md5(body).digest()
-        
-    def compare_checksum(ck1, ck2) :
-        return ck1 == ck2
-    
-
